b05cacccef3e593c47aba052855b75459d550923
kgatatsoo <vibrationbanda@gmail.com>
2026-01-02 19:38:30 +0200

    Deleted to start afresh

--- Libraryproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # make sure include is imported
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('library.urls')),  # /api/ requests go to library app
-]
